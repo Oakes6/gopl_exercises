bf569719ddf682c1cfb08cd7400f7c1a5c409dc1
Tanner Oakes <tanner.w.oakes@gmail.com>
2020-07-09 20:41:40 -0500

    finish ex7.11

---
+++ ex7.11/http4.go
@@ -0,0 +1,114 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 195.
+
+// Http4 is an e-commerce server that registers the /list and /price
+// endpoint by calling http.HandleFunc.
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"sync"
+)
+
+var mutex *sync.Mutex = &sync.Mutex{}
+
+//!+main
+
+func main() {
+
+	db := database{"shoes": 50, "socks": 5}
+	http.HandleFunc("/list", db.list)
+	http.HandleFunc("/create", db.create)
+	http.HandleFunc("/delete", db.delete)
+	http.HandleFunc("/price", db.price)
+	http.HandleFunc("/update", db.update)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+//!-main
+
+type dollars float32
+
+func (d dollars) String() string { return fmt.Sprintf("$%.2f", d) }
+
+type database map[string]dollars
+
+func (db database) list(w http.ResponseWriter, req *http.Request) {
+	for item, price := range db {
+		fmt.Fprintf(w, "%s: %s\n", item, price)
+	}
+}
+
+func (db database) price(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	if price, ok := db[item]; ok {
+		fmt.Fprintf(w, "%s\n", price)
+	} else {
+		w.WriteHeader(http.StatusNotFound) // 404
+		fmt.Fprintf(w, "no such item: %q\n", item)
+	}
+}
+
+func (db database) update(w http.ResponseWriter, r *http.Request) {
+	item := r.URL.Query().Get("item")
+	price := r.URL.Query().Get("price")
+	if item == "" || price == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Must provide params 'item' and 'price'")
+	}
+	_, ok := db[item]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "Item not present in the db")
+	}
+	intPrice, err := strconv.Atoi(price)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Not a valid value for price")
+	}
+	mutex.Lock()
+	db[item] = dollars(intPrice)
+	mutex.Unlock()
+}
+
+func (db database) delete(w http.ResponseWriter, r *http.Request) {
+	item := r.URL.Query().Get("item")
+	if item == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Must use item parameter")
+	}
+	_, ok := db[item]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "Item not found")
+	}
+	mutex.Lock()
+	delete(db, item)
+	mutex.Unlock()
+	w.WriteHeader(http.StatusOK)
+}
+
+func (db database) create(w http.ResponseWriter, r *http.Request) {
+	item := r.URL.Query().Get("item")
+	price := r.URL.Query().Get("price")
+	_, ok := db[item]
+	if ok {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Item already exists")
+	} else {
+		parsedPrice, err := strconv.ParseFloat(price, 64)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(w, "failed to parse string to float")
+		}
+		mutex.Lock()
+		db[item] = dollars(parsedPrice)
+		mutex.Unlock()
+		w.WriteHeader(http.StatusCreated)
+	}
+}
